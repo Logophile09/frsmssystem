6c85ba6e182f742394aa35946cc2a90602bf472a
Jamszkie <sasukesensei06@gmail.com>
2026-08-01 04:00:27 +0800

    Fix TypeScript config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
+// @ts-ignore: allow importing CSS as a side effect in this entry file
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
